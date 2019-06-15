2d281e65159857075888878029cadb3de3a09692
hunfatal <fatal07@gmail.com>
2019-06-15 18:07:38 +0200

    Fix constant Proximity Sensor usage
    Proximity sensor was registered, even when there was no action set or active edge was disabled when screen is off.

--- systemui/src/com/android/systemui/elmyra/gates/KeyguardProximity.java
+++ systemui/src/com/android/systemui/elmyra/gates/KeyguardProximity.java
@@ -1,10 +1,13 @@
 package com.google.android.systemui.elmyra.gates;
 
+import android.os.UserHandle;
 import android.content.Context;
+import android.content.ContentResolver;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.provider.Settings;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.util.AsyncSensorManager;
@@ -19,6 +22,7 @@ public class KeyguardProximity extends Gate {
     private final SensorManager mSensorManager = ((SensorManager) Dependency.get(AsyncSensorManager.class));
     private final Sensor mSensor = mSensorManager.getDefaultSensor(8);
     private final SensorEventListener mSensorListener = new SensorListenerImpl();
+    private final ContentResolver mResolver;
 
     private class KeyguardProximityListenerImpl implements Listener {
         KeyguardProximityListenerImpl() {
@@ -50,6 +54,7 @@ public class KeyguardProximity extends Gate {
 
     public KeyguardProximity(Context context) {
         super(context);
+        mResolver = context.getContentResolver();
         if (mSensor == null) {
             mProximityThreshold = 0.0f;
             mKeyguardGate = null;
@@ -60,16 +65,22 @@ public class KeyguardProximity extends Gate {
         mKeyguardGate.setListener(mGateListener);
         updateProximityListener();
     }
+    
+    private boolean isActiveEdgeEnabled() {
+        return (Settings.Secure.getIntForUser(mResolver, Settings.Secure.LONG_SQUEEZE_SELECTION, 0, UserHandle.USER_CURRENT) > 0
+            || Settings.Secure.getIntForUser(mResolver, Settings.Secure.SHORT_SQUEEZE_SELECTION, 0, UserHandle.USER_CURRENT) > 0)
+            && Settings.Secure.getIntForUser(mResolver, Settings.Secure.ASSIST_GESTURE_WAKE_ENABLED, 1, UserHandle.USER_CURRENT) == 1;
+    }
 
     private void updateProximityListener() {
         if (mProximityBlocked) {
             mProximityBlocked = false;
             notifyListener();
         }
-        if (!isActive() || !mKeyguardGate.isKeyguardShowing() || mKeyguardGate.isKeyguardOccluded()) {
+        if (!isActive() || !mKeyguardGate.isKeyguardShowing() || mKeyguardGate.isKeyguardOccluded() || !isActiveEdgeEnabled()) {
             mSensorManager.unregisterListener(mSensorListener);
             mIsListening = false;
-        } else if (!mIsListening) {
+        } else if (!mIsListening && isActiveEdgeEnabled()) {
             mSensorManager.registerListener(mSensorListener, mSensor, 3);
             mIsListening = true;
         }
